6cc4872392cecc5aa179ce427dc711b90cf1af20
Dr3w7h3is <drew.theis.1@gmail.com>
2019-10-03 14:18:09 -0700

    updates

---
+++ my_math.py
@@ -0,0 +1,34 @@
+print("Hens", 25 + 30 / 5)
+print(10*(10-25))
+print(10%9)
+print(5%5)
+print(100%5)
+print(3 + 2 < 5 -7)
+print(3 + 2 <= 5 - 7)
+print(3 + 2 != 5)
+
+
+a = True
+print(a)
+
+hi = "Hello world"
+sum = 5 + 10
+print(str(sum) + str(sum))
+print(sum + "" + sum)
+print(sum)
+print("The sum is", sum, " big")
+
+my_name = "Drew"
+my_age = 29
+my_height = 72
+my_weight = 240
+my_eyes = "blue"
+my_teeth = "white"
+my_hair = "blonde"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inces tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actucall that's not too heavy.")
+print(f"He's got {my_eyes} eyes and my {my_hair} hair".)
+print(f"jjj")
